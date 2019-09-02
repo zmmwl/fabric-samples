9f1a9e3f9872ccb93d6e379c4b3542f1491a91dc
zmmwl <zmmwl@aliyun.com>
2019-09-02 23:23:36 +0800

    zmm

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -70,49 +70,49 @@ joinChannel () {
 	done
 }
 
-# ## Create channel
-# echo "Creating channel..."
-# createChannel
+## Create channel
+echo "Creating channel..."
+createChannel
 
-# ## Join all the peers to the channel
-# echo "Having all peers join the channel..."
-# joinChannel
+## Join all the peers to the channel
+echo "Having all peers join the channel..."
+joinChannel
 
-# ## Set the anchor peers for each org in the channel
-# echo "Updating anchor peers for org1..."
-# updateAnchorPeers 0 1
-# echo "Updating anchor peers for org2..."
-# updateAnchorPeers 0 2
+## Set the anchor peers for each org in the channel
+echo "Updating anchor peers for org1..."
+updateAnchorPeers 0 1
+echo "Updating anchor peers for org2..."
+updateAnchorPeers 0 2
 
-# if [ "${NO_CHAINCODE}" != "true" ]; then
+if [ "${NO_CHAINCODE}" != "true" ]; then
 
-# 	## Install chaincode on peer0.org1 and peer0.org2
-# 	echo "Installing chaincode on peer0.org1..."
-# 	installChaincode 0 1
-# 	echo "Install chaincode on peer0.org2..."
-# 	installChaincode 0 2
+	## Install chaincode on peer0.org1 and peer0.org2
+	echo "Installing chaincode on peer0.org1..."
+	installChaincode 0 1
+	echo "Install chaincode on peer0.org2..."
+	installChaincode 0 2
 
-# 	# Instantiate chaincode on peer0.org2
-# 	echo "Instantiating chaincode on peer0.org2..."
-# 	instantiateChaincode 0 2
+	# Instantiate chaincode on peer0.org2
+	echo "Instantiating chaincode on peer0.org2..."
+	instantiateChaincode 0 2
 
-# 	# Query chaincode on peer0.org1
-# 	echo "Querying chaincode on peer0.org1..."
-# 	chaincodeQuery 0 1 100
+	# Query chaincode on peer0.org1
+	echo "Querying chaincode on peer0.org1..."
+	chaincodeQuery 0 1 100
 
-# 	# Invoke chaincode on peer0.org1 and peer0.org2
-# 	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
-# 	chaincodeInvoke 0 1 0 2
+	# Invoke chaincode on peer0.org1 and peer0.org2
+	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+	chaincodeInvoke 0 1 0 2
 	
-# 	## Install chaincode on peer1.org2
-# 	echo "Installing chaincode on peer1.org2..."
-# 	installChaincode 1 2
+	## Install chaincode on peer1.org2
+	echo "Installing chaincode on peer1.org2..."
+	installChaincode 1 2
 
-# 	# Query on chaincode on peer1.org2, check if the result is 90
-# 	echo "Querying chaincode on peer1.org2..."
-# 	chaincodeQuery 1 2 90
+	# Query on chaincode on peer1.org2, check if the result is 90
+	echo "Querying chaincode on peer1.org2..."
+	chaincodeQuery 1 2 90
 	
-# fi
+fi
 
 echo
 echo "========= All GOOD, BYFN execution completed =========== "
